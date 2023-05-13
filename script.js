2b6aa9a1e1ceeb8d6eea3bb29e291c2b0440cff2
bkolomiets <127040181+bkolomiets@users.noreply.github.com>
2023-05-13 22:16:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let page = document.querySelector('.body');
+let themeButton = document.querySelector('.themeButton');
+
+themeButton.onclick = function() {
+    page.classList.toggle('lightTheme');
+    page.classList.toggle('darkTheme');
+}
+
+
+.icon1{
+    background-color: #252525;
+}
